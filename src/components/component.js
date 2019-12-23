e82962c1f3aa5a8916c261265d282407d70b17e3
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2019-12-23 20:51:37 +0900

    move to src/

--- src/components/component.js
+++ src/components/component.js
@@ -1,29 +1,34 @@
 import React from 'react';
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       txt: 'this is the state text',
     };
   }
 
-  update (e) {
+  update(e) {
     this.setState({
       txt: e.target.value,
-    })
+    });
   }
 
-  render(){
-    return(
+  render() {
+    const { txt } = this.state;
+
+    return (
       <div>
-        <p>Text: {this.state.txt}</p>
+        <p>
+          Text:
+          {txt}
+        </p>
         <input name="" type="text" onChange={this.update.bind(this)} />
       </div>
-    )
+    );
   }
 }
 
-export const Person = (props) => <h4>{props.name}</h4>;
+export const Person = props => <h4>{props.name}</h4>;
 
 export default App;
